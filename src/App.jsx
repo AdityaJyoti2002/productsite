1442b839f453ff117bdee044bc94af5c48611c41
Aditya Jyoti <119758277+AdityaJyoti2002@users.noreply.github.com>
2025-03-28 03:37:20 +0530

    Updated feature or bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './Pages/Home/home'
+import Home from './Pages/home'
 
 function App() {
   
 
   return (
     <div>
-    <Home/>
+      <Home/>
+    
 
     </div>
 
