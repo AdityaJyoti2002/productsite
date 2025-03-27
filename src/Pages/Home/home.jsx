f6c48c201de410d93f8182fd2f1fe51899f3f699
Aditya Jyoti <119758277+AdityaJyoti2002@users.noreply.github.com>
2025-03-28 03:35:20 +0530

    Updated feature or bug fix

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import NavBar from '../../layout/narbar'
 import Header from '../../Components/Header/Header'
 import Product from '../../Components/Product/product'
 import ProductComparison from '../../Components/ProductComparision/Productcomparision'
@@ -7,19 +6,20 @@ import ContactPage from '../../Components/ContactUs/Contact'
 import Footer from '../../layout/Footer/Footer'
 import Features from '../../Components/Feature/Features'
 import VideoCover from '../../Components/Video/videocard'
+import Navbar from '../../layout/Navbar'
+// import Navbar from '../layout/Navbar'
 
 const Home = () => {
   return (
     <div>
-      <NavBar/>
       {/* <ProductPage/> */}
-    
+    <Navbar/>
       <Header/>
       <VideoCover/>
       <Features/>
       <Product/>
       {/* <Store/> */}
-      {/* <ProductCard1/> */}
+      {/* <ProductCard1/> */}x
       
       <ProductComparison/>
       {/* <ProductLandingPage/> */}
